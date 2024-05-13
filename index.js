a500c28610e15755d7a14746190644535d945147
meghna7838 <meghnatandon7838@gmail.com>
2024-05-13 22:43:22 +0530

    Using babel to make code more declarative

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-const reactElement = React.createElement("h1",{id:"Heading"}," HELLO PEEPS");
+// const reactElement = React.createElement("h1",{id:"Heading"}," HELLO PEEPS");
 
-ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
+// ReactDOM.createRoot(document.getElementById("root")).render(reactElement);
+
+//This is imperative way/style of writing code
+// introducing JSX where we will even make this code shorter
+const jsxElt = (<h1>Hello World!!</h1>);
+ReactDOM.createRoot(document.getElementById("root")).render(jsxElt);
+//Problem is this will give error saying it doesnt understand <> as browser doesnt understand html so overcome this we will add Babel a compiler used to convert html to equivalent js
+//So in html file use CDN and add type="text/babel in script tag"
