04e6430e302cf1ac996dbe2c0fdd349538ce636e
meghna7838 <meghnatandon7838@gmail.com>
2024-05-13 23:30:13 +0530

    React Fragments

--- index.js
+++ index.js
@@ -4,7 +4,16 @@
 
 //This is imperative way/style of writing code
 // introducing JSX where we will even make this code shorter
-const jsxElt = (<h1>Hello World!!</h1>);
+const jsxElt =  <React.Fragment>
+                <h1>Hello World!!</h1>
+                <p> This is lob </p>
+                <h2>All About React</h2>
+                <ul>
+                    <li>Declarative</li>
+                    <li>Single Page Application</li>
+                </ul>
+                </React.Fragment>
+                
 ReactDOM.createRoot(document.getElementById("root")).render(jsxElt);
 //Problem is this will give error saying it doesnt understand <> as browser doesnt understand html so overcome this we will add Babel a compiler used to convert html to equivalent js
 //So in html file use CDN and add type="text/babel in script tag"
